b50ec7abf00d022f3e63aabf35a2311b379bfeda
Hmong Moua <moua0390@umn.edu>
2023-11-20 15:37:43 -0600

    Created `trim_reads.sh` for Trimmomatic execution

---
+++ trim_reads.sh
@@ -0,0 +1,16 @@
+mkdir trimmed_reads
+
+read1=sars_spike_protein_raw_reads_1.fastq
+read2=sars_spike_protein_raw_reads_2.fastq
+
+output1_paired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_paired.fastq
+output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.fastq
+
+output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fastq
+output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fastq
+
+java -jar trimmomatic/trimmomatic-0.39.jar PE $read1 $read2 \
+$output1_paired $output1_unpaired \
+$output2_paired $output2_unpaired \
+ILLUMINACLIP:trimmomatic/adapters/TruSeq3-PE.fa:2:30:10:2:True \
+LEADING:3 TRAILING:3 MINLEN:36
