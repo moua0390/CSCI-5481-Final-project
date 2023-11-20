cf84f760fdaed3affbb3efb612dc6f6d25ff825f
Hmong Moua <moua0390@umn.edu>
2023-11-20 15:54:05 -0600

    Added to script to generate log file

--- trim_reads.sh
+++ trim_reads.sh
@@ -9,7 +9,9 @@ output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.f
 output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fastq
 output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fastq
 
-java -jar trimmomatic/trimmomatic-0.39.jar PE $read1 $read2 \
+logfile=trimmed_reads/log.txt
+
+java -jar trimmomatic/trimmomatic-0.39.jar PE -trimlog $logfile $read1 $read2 \
 $output1_paired $output1_unpaired \
 $output2_paired $output2_unpaired \
 ILLUMINACLIP:trimmomatic/adapters/TruSeq3-PE.fa:2:30:10:2:True \
