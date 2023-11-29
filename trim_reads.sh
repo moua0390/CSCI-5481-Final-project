c5aca21ac7253972a067300b5d9c09b5fe27a09a
Hmong Moua <moua0390@umn.edu>
2023-11-28 22:16:26 -0600

    Added FastQC to determine Trimmomatic arguments

--- trim_reads.sh
+++ trim_reads.sh
@@ -1,24 +1,25 @@
-mkdir trimmed_reads
+mkdir -p trimmed_reads
 
-read1=sars_spike_protein_raw_reads_1.fq
-read2=sars_spike_protein_raw_reads_2.fq
+read1=sars_spike_protein_raw_reads_1.fastq
+read2=sars_spike_protein_raw_reads_2.fastq
 
 head -n 10000 sars_spike_protein_raw_reads.fastq > $read1
 tail -n 10000 sars_spike_protein_raw_reads.fastq > $read2
 
-gzip $read1
-gzip $read2
+gzip -f $read1 $read2
 
-output1_paired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_paired.fq.gz
-output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.fq.gz
+output1_paired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_paired.fastq.gz
+output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.fastq.gz
 
-output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fq.gz
-output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fq.gz
+output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fastq.gz
+output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fastq.gz
 
 logfile=trimmed_reads/log.txt
+statsfile=trimmed_reads/stats.txt
 
-java -jar trimmomatic/trimmomatic-0.39.jar PE -trimlog $logfile $read1.gz $read2.gz \
+java -jar packages/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 4 -phred33 \
+-trimlog $logfile -summary $statsfile \
+$read1.gz $read2.gz \
 $output1_paired $output1_unpaired \
 $output2_paired $output2_unpaired \
-ILLUMINACLIP:trimmomatic/adapters/TruSeq3-PE-2.fa:2:30:10:2:True \
-LEADING:3 TRAILING:3 MINLEN:36
+SLIDINGWINDOW:4:25 LEADING:35 TRAILING:35 MINLEN:35
