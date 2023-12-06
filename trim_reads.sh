252b2417c3efc91de91b00508bfb4ec416bbe9e0
Hmong Moua <moua0390@umn.edu>
2023-12-06 10:40:02 -0600

    Unzipped files

--- trim_reads.sh
+++ trim_reads.sh
@@ -6,20 +6,18 @@ read2=sars_spike_protein_raw_reads_2.fastq
 head -n 10000 sars_spike_protein_raw_reads.fastq > $read1
 tail -n 10000 sars_spike_protein_raw_reads.fastq > $read2
 
-gzip -f $read1 $read2
+output1_paired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_paired.fastq
+output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.fastq
 
-output1_paired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_paired.fastq.gz
-output1_unpaired=trimmed_reads/sars_spike_protein_raw_reads_1_trimmed_unpaired.fastq.gz
-
-output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fastq.gz
-output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fastq.gz
+output2_paired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_paired.fastq
+output2_unpaired=trimmed_reads/sars_spike_protein_raw_reads_2_trimmed_unpaired.fastq
 
 logfile=trimmed_reads/log.txt
 statsfile=trimmed_reads/stats.txt
 
 java -jar packages/Trimmomatic-0.39/trimmomatic-0.39.jar PE -threads 4 -phred33 \
 -trimlog $logfile -summary $statsfile \
-$read1.gz $read2.gz \
+$read1 $read2 \
 $output1_paired $output1_unpaired \
 $output2_paired $output2_unpaired \
 SLIDINGWINDOW:4:25 LEADING:35 TRAILING:35 MINLEN:35
