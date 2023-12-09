ea491fa3b84b63601bedee26b38d4dcd625d6a17
parsamoradi <parsa.moradi73@gmail.com>
2023-12-09 11:56:08 -0600

    change lighter parameteres

--- trim_reads.sh
+++ trim_reads.sh
@@ -11,4 +11,4 @@ java -jar packages/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 4 -phred33 
 SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3 MINLEN:35
 
 mkdir -p lighter_out
-Lighter_exec/lighter -r trimmed_reads/sars_spike_protein_raw_reads_trimmed.fastq -K 5 5000 -od lighter_out/
+Lighter_exec/lighter -r trimmed_reads/sars_spike_protein_raw_reads_trimmed.fastq -k 17 5000 0.035 -t 10 -od lighter_out/
