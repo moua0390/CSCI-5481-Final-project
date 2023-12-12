d12497f3e732c2f8d53c3125cb0f4bade34a3848
Hmong Moua <moua0390@umn.edu>
2023-12-12 00:09:49 -0600

    Renamed folders for uniformity

--- run_fastqc.sh
+++ run_fastqc.sh
@@ -4,4 +4,4 @@ raw_reads=sars_spike_protein_raw_reads.fastq
 trimmed_reads=trimmed_reads/sars_spike_protein_raw_reads_trimmed.fastq
 corrected_reads=lighter_out/sars_spike_protein_raw_reads_trimmed.cor.fq
 
-packages/FastQC/fastqc $raw_reads $trimmed_reads $corrected_reads -o fastqc_output
+packages/FastQC/fastqc $raw_reads $trimmed_reads $corrected_reads -o fastqc_out
--- trim_reads.sh
+++ trim_reads.sh
@@ -1,14 +1,14 @@
-mkdir -p trimmed_reads
+mkdir -p trim_out
 
 input=sars_spike_protein_raw_reads.fastq
-output=trimmed_reads/sars_spike_protein_raw_reads_trimmed.fastq
+output=trim_out/sars_spike_protein_raw_reads_trimmed.fastq
 
-logfile=trimmed_reads/log.txt
-statsfile=trimmed_reads/stats.txt
+logfile=trim_out/log.txt
+statsfile=trim_out/stats.txt
 
 java -jar packages/Trimmomatic-0.39/trimmomatic-0.39.jar SE -threads 4 -phred33 \
 -trimlog $logfile -summary $statsfile $input $output \
 SLIDINGWINDOW:4:15 LEADING:3 TRAILING:3 MINLEN:35
 
 mkdir -p lighter_out
-packages/Lighter/lighter -r trimmed_reads/sars_spike_protein_raw_reads_trimmed.fastq -k 17 5000 0.035 -t 10 -od lighter_out/
+packages/Lighter/lighter -r trim_out/sars_spike_protein_raw_reads_trimmed.fastq -k 17 5000 0.035 -t 10 -od lighter_out/
